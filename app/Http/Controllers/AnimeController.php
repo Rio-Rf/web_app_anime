0b89acbe48500be70260994b8393bf81d114c913
Rio-Rf <rio141421356@icloud.com>
2023-06-03 19:20:19 +0900

    [修正]とを使わない実装，クォート問題の解決

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -139,19 +139,12 @@ class AnimeController extends Controller
     }
     public function ranking(Request $request)
     {
-        //一時テーブルでlikeカラムの合計値をlike_countに保存
-        DB::statement("
-            CREATE TEMPORARY TABLE temp_like_counts
-            SELECT anime_id, SUM(`like`) AS like_count
-            FROM anime_users
-            GROUP BY anime_id
-        ");
-        // アニメテーブルと一時テーブルを結合し、like_countで降順に並び替えて結果を取得する
-        $animeRanks = Anime::leftJoin('temp_like_counts', 'animes.id', '=', 'temp_like_counts.anime_id')
-            ->orderBy('temp_like_counts.like_count', 'desc')
+        //likeカラムの合計値をlike_countカラムに保存, 実在するanime_usersテーブルを直接指定して操作している
+        $animeRanks = Anime::select('animes.*', DB::raw('SUM(anime_users.like) AS like_count'))
+            ->leftJoin('anime_users', 'animes.id', '=', 'anime_users.anime_id')
+            ->groupBy('animes.id', 'animes.title', 'animes.on_air_season', 'img_path', 'official_url', 'created_at', 'updated_at', 'deleted_at') // グループ化する列
+            ->orderByDesc('like_count')
             ->paginate(10);
-        // 一時テーブルを削除する
-        DB::statement("DROP TEMPORARY TABLE IF EXISTS temp_like_counts");
         //dd($animeRanks);
         
         $user_id = $request->user()->id;
@@ -192,19 +185,12 @@ class AnimeController extends Controller
                         ->where('user_id', $user_id)
                         ->update(['like' => 1]);
         }
-        //一時テーブルでlikeカラムの合計値をlike_countに保存
-        DB::statement("
-            CREATE TEMPORARY TABLE temp_like_counts
-            SELECT anime_id, SUM(`like`) AS like_count
-            FROM anime_users
-            GROUP BY anime_id
-        ");
-        // アニメテーブルと一時テーブルを結合し、like_countで降順に並び替えて結果を取得する
-        $animeRanks = Anime::leftJoin('temp_like_counts', 'animes.id', '=', 'temp_like_counts.anime_id')
-            ->orderBy('temp_like_counts.like_count', 'desc')
+        //likeカラムの合計値をlike_countカラムに保存，実在するanime_usersテーブルを直接指定して操作している
+        $animeRanks = Anime::select('animes.*', DB::raw('SUM(anime_users.like) AS like_count'))
+            ->leftJoin('anime_users', 'animes.id', '=', 'anime_users.anime_id')
+            ->groupBy('animes.id', 'animes.title', 'animes.on_air_season', 'img_path', 'official_url', 'created_at', 'updated_at', 'deleted_at') // グループ化する列
+            ->orderByDesc('like_count')
             ->paginate(10);
-        // 一時テーブルを削除する
-        DB::statement("DROP TEMPORARY TABLE IF EXISTS temp_like_counts");
         //dd($animeRanks);
         
         $anime_users = Anime_user::where('user_id', $user_id)->where('like', 1)->get();
@@ -221,22 +207,17 @@ class AnimeController extends Controller
         Anime_user::where('anime_id', $anime_id)//中間テーブルの操作はこの方法が適しているようだ
                     ->where('user_id', $user_id)
                     ->update(['like' => 0]);
-        //一時テーブルでlikeカラムの合計値をlike_countに保存
-        DB::statement("
-            CREATE TEMPORARY TABLE temp_like_counts
-            SELECT anime_id, SUM(`like`) AS like_count
-            FROM anime_users
-            GROUP BY anime_id
-        ");
-        // アニメテーブルと一時テーブルを結合し、like_countで降順に並び替えて結果を取得する
-        $animeRanks = Anime::leftJoin('temp_like_counts', 'animes.id', '=', 'temp_like_counts.anime_id')
-            ->orderBy('temp_like_counts.like_count', 'desc')
+                        
+        //likeカラムの合計値をlike_countカラムに保存，実在するanime_usersテーブルを直接指定して操作している
+        $animeRanks = Anime::select('animes.*', DB::raw('SUM(anime_users.like) AS like_count'))
+            ->leftJoin('anime_users', 'animes.id', '=', 'anime_users.anime_id')
+            ->groupBy('animes.id', 'animes.title', 'animes.on_air_season', 'img_path', 'official_url', 'created_at', 'updated_at', 'deleted_at') // グループ化する列
+            ->orderByDesc('like_count')
             ->paginate(10);
-        // 一時テーブルを削除する
-        DB::statement("DROP TEMPORARY TABLE IF EXISTS temp_like_counts");
         //dd($animeRanks);
         
         $anime_users = Anime_user::where('user_id', $user_id)->where('like', 1)->get();
+        //dd($anime_users);
         
         $before_like_count = $request->query('before_like_count');//前ページの変数を継承
         $count = $request->query('count');//前ページの順位の変数を継承
